51e1c59411262963c0d92334b2da93e2b29af271
Dardrich <dardrich1465@gmail.com>
2025-02-06 21:33:35 +0700

    feat: add edit Product

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -11,9 +11,26 @@ import java.util.Iterator;
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
+    public Product findById(String productId) {
+        return productData.stream()
+                .filter(product -> product.getProductId().equals(productId))
+                .findFirst()
+                .orElse(null);
+    }
+
     public Product create(Product product) {
         productData.add(product);
         return product;
+    }
+
+    public void edit(Product editedProduct) {
+        for (int i = 0; i < productData.size(); i++) {
+            Product product = productData.get(i);
+            if (product.getProductId().equals(editedProduct.getProductId())) {
+                productData.set(i, editedProduct);
+                break;
+            }
+        }
     }
 
     public Iterator<Product> findAll() {
