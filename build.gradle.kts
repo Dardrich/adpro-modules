cda05546fcee48b03cb904b3114df8ebfb744f41
Dardrich <dardrich1465@gmail.com>
2025-02-10 20:36:14 +0700

    chore: remove mistype char

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
     testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
     testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")g
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
 }
 
 tasks.register<Test>("unitTest") {
